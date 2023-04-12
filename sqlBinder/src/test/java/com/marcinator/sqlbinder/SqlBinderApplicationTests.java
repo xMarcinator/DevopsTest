62cbdf66b726b046a1e0489f0f4512491ec7ab74
marc <marcjensenvirklund@gmail.com>
2023-04-12 10:20:26 +0200

    reworked java project to use spring web

---
+++ sqlBinder/src/test/java/com/marcinator/sqlbinder/SqlBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinator.sqlbinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
